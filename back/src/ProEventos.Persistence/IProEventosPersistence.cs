6079e011329d144554a770f647f1f624a0143a09
leonardo <leolds010101@gmail.com>
2022-03-17 15:50:40 -0300

    seção 6 - .NET5 - Múltiplas Camadas

--- back/src/ProEventos.Persistence/IProEventosPersistence.cs
+++ back/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -6,6 +6,8 @@ namespace ProEventos.Persistence
     public interface IProEventosPersistence
     {
         //GERAL
+        //T é generico
+        //todo e qualquer add,atualizar ou deletar que tivermos, e salvar, vai ser utilizado esses métodos
         void Add<T>(T entity) where T:class;
         void Update<T>(T entity) where T:class;
         void Delete<T>(T entity) where T:class;
