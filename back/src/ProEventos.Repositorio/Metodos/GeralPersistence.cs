3eefae32f7310c3182e00c6538a3e1ef4d991c62
leonardo <leolds010101@gmail.com>
2022-03-17 16:46:52 -0300

    seção 6 - .NET5 - Múltiplas Camadas

---
+++ back/src/ProEventos.Repositorio/Metodos/GeralPersistence.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Persistence.Metodos
+{
+    public class GeralPersistence   : IGeralPersistence
+    {
+        private readonly ProEventosContext context;
+
+        public GeralPersistence(ProEventosContext context)
+        {
+            this.context = context;
+        }
+        
+        public void Add<T>(T entity) where T : class
+        {
+            this.context.Add(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            this.context.Update(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            this.context.Remove(entity);
+        }
+
+        public void DeleteRange<T>(T[] entityArray) where T : class
+        {
+            this.context.RemoveRange(entityArray);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await this.context.SaveChangesAsync()) > 0;
+        }
+
+    }
+}
