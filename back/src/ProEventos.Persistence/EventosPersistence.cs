ee37923cc8c5d3770f4c37d34aa1876ebea1e2b9
leonardo <leolds010101@gmail.com>
2022-03-18 17:54:13 -0300

    seção 6 - .NET5 - Múltiplas Camadas (62 complet)

--- back/src/ProEventos.Persistence/EventosPersistence.cs
+++ back/src/ProEventos.Persistence/EventosPersistence.cs
@@ -13,32 +13,7 @@ namespace ProEventos.Persistence
         public EventosPersistence(ProEventosContext context)
         {
             this.context = context;
-        }
-        
-        public void Add<T>(T entity) where T : class
-        {
-            this.context.Add(entity);
-        }
-
-        public void Update<T>(T entity) where T : class
-        {
-            this.context.Update(entity);
-        }
-
-        public void Delete<T>(T entity) where T : class
-        {
-            this.context.Remove(entity);
-        }
-
-        public void DeleteRange<T>(T[] entityArray) where T : class
-        {
-            this.context.RemoveRange(entityArray);
-        }
-
-        public async Task<bool> SaveChangesAsync()
-        {
-            return (await this.context.SaveChangesAsync()) > 0;
-        }
+        }        
 
 //------------------------------------- EVENTO -----------------------------------------------\\  
 
@@ -83,49 +58,6 @@ namespace ProEventos.Persistence
             return await query.FirstOrDefaultAsync();
         }
 
-//------------------------------------- PALESTRANTES -----------------------------------------------\\  
-        public async Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos = false)
-        {
-            IQueryable<Palestrante> query = this.context.Palestrantes.Include(p => p.RedesSociais);
-                //se o IncludeEventos for positivo, ele irá entrar no IF para adicionar Evento no PalestranteEventos
-            if(includeEventos){
-            //Dado o PalestranteEventos, Irá ser adicionado o Palestrante
-            query = query.Include(p => p.PalestrantesEventos).ThenInclude(pe => pe.Evento);
-            }
-            //ordenar Pelo Id
-            query = query.OrderBy(p => p.Id);
-
-            return await query.ToArrayAsync();
-        }
-
-        public async Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos = false)
-        {
-            IQueryable<Palestrante> query = this.context.Palestrantes.Include(p => p.RedesSociais);
-                //se o IncludeEventos for positivo, ele irá entrar no IF para adicionar Evento no PalestranteEventos
-            if(includeEventos){
-            //Dado o PalestranteEventos, Irá ser adicionado o Palestrante
-            query = query.Include(p => p.PalestrantesEventos).ThenInclude(pe => pe.Evento);
-            }
-                                    //A cada evento que tiver, procura o tema, converte para lower e analisa se contem um tema convertido em lower 
-            query = query.OrderBy(p => p.Id).Where(p => p.Nome.ToLower().Contains(nome.ToLower()));
-
-            return await query.ToArrayAsync();
-        }
-
-
-        public async Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool includeEventos = false)
-        {
-            IQueryable<Palestrante> query = this.context.Palestrantes.Include(p => p.RedesSociais);
-                //se o IncludeEventos for positivo, ele irá entrar no IF para adicionar Evento no PalestranteEventos
-            if(includeEventos){
-            //Dado o PalestranteEventos, Irá ser adicionado o Palestrante
-            query = query.Include(p => p.PalestrantesEventos).ThenInclude(pe => pe.Evento);
-            }
-                                    
-            query = query.OrderBy(p => p.Id).Where(p => p.Id == palestranteId);
-                                //retornando apenas 1   
-            return await query.FirstOrDefaultAsync();
-        }
 
 
     }
